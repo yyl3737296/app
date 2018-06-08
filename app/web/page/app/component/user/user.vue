e664a170588b74bb66d059111c320e3e18ac89ba
Aaron <297303831@qq.com>
2018-06-08 16:22:46 +0800

    table

--- app/web/page/app/component/user/user.vue
+++ app/web/page/app/component/user/user.vue
@@ -1,9 +1,39 @@
 <template>
-  <Datatable></Datatable>
+  <div class="card">
+    <div class="card-header">
+      <h3 class="card-title">用户管理</h3>
+    </div>
+    <div class="card-body">
+      <Datatable id="user_table" :data="tabledata">
+      </Datatable>
+    </div>
+  </div>
 </template>
 <style>
 
 </style>
 <script type="text/babel">
-
+export default {
+  data(){
+    return {
+      tabledata: {
+        head:[
+          'a',
+          'b',
+          'c',
+          'd',
+          'e'
+        ]
+      }
+    }
+  },
+  computed: {
+  },
+  methods: {
+    logOut() {
+    }
+  },
+  mounted() {
+  }
+};
 </script>
